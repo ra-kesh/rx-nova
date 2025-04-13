530fc58c5a58b7599cb43f250976999733de55be
ra-kesh <iamrakeshkumar@pm.me>
2025-04-13 10:31:38 +0530

    initial commmit-start-from-scratch

---
+++ app.config.ts
@@ -0,0 +1,16 @@
+// app.config.ts
+import { defineConfig } from '@tanstack/react-start/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+    server: {
+        preset: 'netlify',
+      },
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+    ],
+  },
+})
