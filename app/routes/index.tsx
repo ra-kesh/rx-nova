43eb7e3a87a288543bfdb887d833768ca7687c7e
ra-kesh <iamrakeshkumar@pm.me>
2025-04-14 08:57:32 +0530

    removed suspense query and extra server function

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,40 +7,26 @@ import {
   SignUpButton,
   useAuth,
 } from "@clerk/tanstack-react-start";
-import { getAuth } from "@clerk/tanstack-react-start/server";
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { getWebRequest } from "@tanstack/react-start/server";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { convexQuery } from "@convex-dev/react-query";
 import { api } from "../../convex/_generated/api";
 
-export const authStateFn = createServerFn({ method: "GET" }).handler(async () => {
-  const request = getWebRequest();
-  if (!request) throw new Error("No request found");
-  const { userId,getToken} = await getAuth(request);
-
-  const token = await getToken({ template: 'convex' })
-
-  if (!userId) {
-    throw redirect({
-      to: "/sign-in/$",
-    });
-  }
-
-  return { userId,token };
-});
 
 export const Route = createFileRoute("/")({
   component: Home,
-  beforeLoad: async () => await authStateFn(),
-  // loader: async ({ context }) => {
-  //   return { userId: context.userId };
-  // },
+  beforeLoad: async ({context}) => {
+    if (!context.userId) {
+      throw redirect({
+        to: "/sign-in/$",
+      });
+    }
+  },
+
 });
 
 function Home() {
-  const { data } = useSuspenseQuery(convexQuery(api.tasks.get, {}));
+  const { data } = useQuery(convexQuery(api.tasks.get, {}));
 
   const {userId} = useAuth();
 
@@ -48,9 +34,8 @@ function Home() {
     <div>
       <h1>Welcome! Your ID is {userId}!</h1>
 
-
       <div>
-      {data.map(({ _id, text }) => (
+      {data?.map(({ _id, text }) => (
         <div key={_id}>{text}</div>
       ))}
     </div>
